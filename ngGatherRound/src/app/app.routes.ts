721e1e2beda60aaa0e28bd64bf52303a5bf2cf34
bulldogg7 <163910022+bulldogg7@users.noreply.github.com>
2025-02-14 15:11:31 -0700

    .

--- ngGatherRound/src/app/app.routes.ts
+++ ngGatherRound/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './componenets/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
