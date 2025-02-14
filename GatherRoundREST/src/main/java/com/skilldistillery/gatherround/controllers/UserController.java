a30372c87626533af190f9446cb88eed74ba1bd6
George <villegas.george96@gmail.com>
2025-02-14 10:52:27 -0800

    commit

--- GatherRoundREST/src/main/java/com/skilldistillery/gatherround/controllers/UserController.java
+++ GatherRoundREST/src/main/java/com/skilldistillery/gatherround/controllers/UserController.java
@@ -1,17 +1,15 @@
 package com.skilldistillery.gatherround.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.gatherround.entities.User;
 import com.skilldistillery.gatherround.services.UserService;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("api")
@@ -22,12 +20,7 @@ public class UserController {
 	private UserService userService;
 	
 	@GetMapping(path="users")
-	public User showAllUsers(User user,
-			HttpServletResponse response) {
-		User foundUsers = userService.show(user.getUsername());
-		if (foundUsers == null) {
-			response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 404
-		}
-		return foundUsers;
+	public List<User> showAllUsers () {
+		return null;
 	}
 }
