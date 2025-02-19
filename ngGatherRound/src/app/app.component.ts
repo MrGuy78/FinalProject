c23a879dcd386abdfba168b07bdcad8f8f5764ed
bulldogg7 <163910022+bulldogg7@users.noreply.github.com>
2025-02-19 14:50:03 -0700

    Added Footer

--- ngGatherRound/src/app/app.component.ts
+++ ngGatherRound/src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent,
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
